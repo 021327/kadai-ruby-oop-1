82d51e1dfdb7e02960daee637fad80af05c81e29
Ryota.Fujitaka <ryouta.fujitaka@icloud.com>
2023-10-05 14:39:24 +0000

    kadai-ruby-oop-1

--- team.rb
+++ team.rb
@@ -10,28 +10,27 @@ class Team
 
   def calc_win_rate
     calc_win_rate = self.win.to_f / (self.win + self.lose)
-  
-    puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{calc_win_rate}です."
+    calc_win_rate.round(3)  # 勝率を小数点以下3桁まで表示
+  end
+
+  def show_team_result
+    win_rate = calc_win_rate
+    puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は #{win_rate} です。"
   end
 end
 
-# チームのインスタンスを生成
-Giants = Team.new('Giants', 67, 45, 8)
-Tigers = Team.new('Tigers', 60, 53, 7)
-Dragons = Team.new('Dragons', 60, 55, 5)
-Baystars = Team.new('Baystars', 56, 58, 6)
-Carp = Team.new('Carp', 52, 56, 12)
-Swallows = Team.new('Swallows', 41, 69, 10)
+# 6 チーム分のインスタンスを生成
+giants = Team.new('Giants', 67, 45, 8)
+tigers = Team.new('Tigers', 60, 53, 7)
+dragons = Team.new('Dragons', 60, 55, 5)
+baystars = Team.new('BayStars', 56, 58, 6)
+carp = Team.new('Carp', 52, 56, 12)
+swallows = Team.new('Swallows', 41, 69, 10)
 
-# 勝率を計算して出力
-Giants.calc_win_rate
-Tigers.calc_win_rate
-Dragons.calc_win_rate
-Baystars.calc_win_rate
-Carp.calc_win_rate
-Swallows.calc_win_rate
-
-
- 
-
-  
+# 各チームの成績を表示
+giants.show_team_result
+tigers.show_team_result
+dragons.show_team_result
+baystars.show_team_result
+carp.show_team_result
+swallows.show_team_result
